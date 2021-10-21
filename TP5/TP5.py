5a680595430ac383fcc2e437dfc0c8015caac3f2
MounaDH <mounaadhaouadi@gmail.com>
2021-10-21 18:30:31 -0400

    Handle UNK words

--- TP5/TP5.py
+++ TP5/TP5.py
@@ -1,6 +1,6 @@
 from nltk.corpus import treebank
 from statistics import mean
-from nltk import Nonterminal
+from nltk import Nonterminal, Production
 from nltk.corpus import treebank
 from nltk import induce_pcfg
 from nltk.parse import ViterbiParser
@@ -27,11 +27,14 @@ def wrong_sentences(file):
     return sentences
 
 
-def main():
-    #install_treebank()  # first time only
+def filter_rules(productions):
+    d = {i: productions.count(i) for i in set(productions)}  # regles , frequencies
+    productions_filtered = [item[0] for item in d.items() if int(item[1]) > 1] # rules that appear > 1 time
+    return productions_filtered
 
-    #Question 3 : train a PCFG grammar using the phrase trees from the PTB corpus
-    # extract productions from three trees and induce the PCFG
+
+def train_PCFG_grammar_using_PTB( filter_by_frequency = False):
+    # extract productions from treebank phrase trees and induce the PCFG grammar
     print("Induce PCFG grammar from treebank data:")
 
     productions = []
@@ -46,36 +49,67 @@ def main():
     print(len(productions))  # 115
     print(productions)
 
-    #filter the grammar, before learning the probabilities : from 115 to 20 regle >1 why do we do it ?
-    d = {i: productions.count(i) for i in set(productions)} # regles , frequencies
-    productions_filtered = [item[0] for item in d.items() if int(item[1]) > 1]
-    print(len(productions_filtered)) # 20 regles
+    grammar_rules = productions
 
+    # FILTER THE GRAMMAR
+    if filter_by_frequency:
+        # filter the grammar, before learning the probabilities : from 115 to 20 regle >1 why do we do it ?
+        productions_filtered = filter_rules(productions)
+        print(len(productions_filtered))  # 20 regles
+        grammar_rules = productions_filtered
+
+    # Handle UNK words 1/2
+    productions_with_UNK = grammar_rules.copy()
+    # pour tout non terminal A intervenant dans une regle lexicale (e.g. A -> the) , ajouter des regles A -> UNK
+    non_terminals_in_lexical_rules = set()  # set does not allow duplicates
+    for p in grammar_rules:
+        if p.is_lexical() and isinstance(p.lhs(), Nonterminal):
+            non_terminals_in_lexical_rules.add(p.lhs())
+
+    for t in non_terminals_in_lexical_rules:
+        new_rule = Production(t, ['UNK'])  # create a new rule
+        productions_with_UNK.append(new_rule)
+
+    # induce the grammar
     S = Nonterminal('S')
-    grammar = induce_pcfg(S, productions)  # different from the one in the doc
+    grammar = induce_pcfg(S, productions_with_UNK)  # different from the one in the doc
     print(grammar)
 
-    # Question 4 : use the grammar to parse grammatically wrong sentences from Cola using ViterbiParser
+    return grammar
+
+
+def parse_sentences(sents, grammar):
     print('Parsing sentences ')
     parser = ViterbiParser(grammar)
-    parser.trace(0) # put 3 for a verbose output
+    parser.trace(0)  # put 3 for a verbose output
 
-    sents_cola = wrong_sentences(Cola_dev_file)
-    for sent in sents_cola:
+    for sent in sents:
         print(sent)
         tokens = sent.split()  # tokenize the sentence
+        print(tokens)
 
+        # Handle UNK words 2/2, todo prb: too many UNK ?
         print('Checking coverage')
-        try:
-            grammar.check_coverage(tokens) # takes a list !!
-            print("All words covered")
-        except:
-            print("Some words not covered")
+        for index, token in enumerate(tokens):
+            try:
+                grammar.check_coverage([token])  # takes a list !!
+            except:
+                tokens[index] = 'UNK'
+        print(tokens)
 
-        parses = parser.parse_all(tokens)     # todo: do something with the parses
-        print(parses)
+        parses_of_a_phrase = parser.parse_all(tokens)  # todo: do something with the parses later ?
+        print(parses_of_a_phrase)
 
 
+def main():
+    #install_treebank()  # first time only
+
+    # Question 3 : train a PCFG grammar using the phrase trees from the PTB corpus
+    grammar = train_PCFG_grammar_using_PTB(filter_by_frequency=False )
+
+    # Question 4 : use the grammar to parse grammatically wrong sentences from Cola using ViterbiParser
+    sents_cola = wrong_sentences(Cola_dev_file)
+    parse_sentences(sents_cola, grammar)
 
 
     # Question 5.B
@@ -101,8 +135,6 @@ def main():
     average_length_PTB = mean(sentences_lengths_PTB)
     print("%.2f" % average_length_PTB )
     print(round(average_length_PTB))  # round it to have exact nb of words
-
-
 
     # Vous pouvez obtenir les arbres de ces phrases comme suit :
     #for item in treebank.fileids():
