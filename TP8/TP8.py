d65ff7a6b0c2da8f8f8f9f0f8d13ff0bce5a58ba
MounaDH <mounaadhaouadi@gmail.com>
2021-11-23 17:55:44 -0500

    initial comment

---
+++ TP8/TP8.py
@@ -0,0 +1,16 @@
+import spacy
+
+
+# pip install transformers
+# pip install sacrebleu
+
+def main():
+    #todo:
+   # utilll.liser transformers ( un code similaire a https://huggingface.co/transformers/task_summary.html#translation
+   # pour trafduire EN---> FR au moins 1000 phrases du corpus WMT.
+   # Vous evaluerez avec  BLEU -- > use library scra bleu
+    pass
+
+
+if __name__ == '__main__':
+    main()
